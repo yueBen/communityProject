6f281add36bb6ae6f81adcd8f8308aa6ad017af3
yueBen <1154451750@qq.com>
2019-02-27 22:26:59 +0800

    wangEdito本地图片上传

--- js/articleEdit.js
+++ js/articleEdit.js
@@ -66,19 +66,20 @@ layui.use('layer',function () {
                 timeout: function (xhr,editor) {
                     // 图片上传超时时触发
                     // xhr 是 XMLHttpRequst 对象，editor 是编辑器对象
-                }
+                },
             // 如果服务器端返回的不是 {errno:0, data: [...]} 这种格式，可使用该配置
             // （但是，服务器端返回的必须是一个 JSON 格式字符串！！！否则会报错）
-            //     customInsert: function (insertImg, result, editor) {
-            //         // 图片上传并返回结果，自定义插入图片的事件（而不是编辑器自动插入图片！！！）
-            //         // insertImg 是插入图片的函数，editor 是编辑器对象，result 是服务器端返回的结果
-            //
-            //         // 举例：假如上传图片成功后，服务器端返回的是 {url:'....'} 这种格式，即可这样插入图片：
-            //         var url = result.url
-            //         insertImg(url)
-            //
-            //         // result 必须是一个 JSON 格式字符串！！！否则报错
-            //     }
+                customInsert: function (insertImg, result, editor) {
+                    // 图片上传并返回结果，自定义插入图片的事件（而不是编辑器自动插入图片！！！）
+                    // insertImg 是插入图片的函数，editor 是编辑器对象，result 是服务器端返回的结果
+
+                    // 举例：假如上传图片成功后，服务器端返回的是 {url:'....'} 这种格式，即可这样插入图片：
+                    console.log(result);
+                    var url = result.data;
+                    insertImg(url);
+
+                    // result 必须是一个 JSON 格式字符串！！！否则报错
+                }
             }
 
         }
