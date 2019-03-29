d071d1ac823e64a3ffd874ddd322d701425efc56
潘俊文 <838326280@qq.com>
2019-03-29 17:34:14 +0800

    3-29

--- js/index.js
+++ js/index.js
@@ -371,10 +371,10 @@ layui.use('layer',function () {
             var checkCode = "";
             var code = ['0','1','2','3','4','5','6','7','8','9',
                 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
-                'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+                'A','B','C','D','E','F','G','H','I','J','K','L','M','N','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
             for (; num > 0; num--) {
-                var i = Math.floor(Math.random() * 62);
+                var i = Math.floor(Math.random() * 61);
                 var r = Math.ceil(Math.random() * 150)+105;
                 var g = Math.ceil(Math.random() * 150)+105;
                 var b = Math.ceil(Math.random() * 150)+105;
