7aee223e1f82f70d4247f787ff930700f61aef3c
潘俊文 <838326280@qq.com>
2019-04-28 17:33:47 +0800

    4-28

--- js/myArticle.js
+++ js/myArticle.js
@@ -67,9 +67,9 @@ layui.use(['laytpl','layer','laydate'],function () {
     function myArticleHtml(list) {
         var html = '';
         $.each(list,function (i,v) {
-            html += '<div class="article-item"><div class="article-item-title">'+ v.title +
+            html += '<div class="article-item" name="' + v.id + '"><div class="article-item-title" name="' + v.id + '">'+ v.title +
                     '</div><div class="article-item-time">'+ toDate(v.updateTime,0) + '</div>'+
-                    '<div class="article-item-btn btn-1" name="' + v.id + '">编辑</div><div class="article-item-btn btn-2" name="' + v.id + '">';
+                    '<div class="article-item-btn btn-1" name="' + v.id + '">编辑</div><div class="article-item-btn btn-2" name="' + v.id + '" value="' + v.status + '">';
 
             if (v.status == 0) {
                 html += '已保存';
@@ -81,6 +81,10 @@ layui.use(['laytpl','layer','laydate'],function () {
                 html += '已发布';
             } else if (v.status == 6) {
                 html += '待发布';
+            } else if (v.status == 7) {
+                html += '已驳回';
+            } else if (v.status == 8) {
+                html += '已通过';
             } else {
                 html += '蛤?';
             }
@@ -131,6 +135,113 @@ layui.use(['laytpl','layer','laydate'],function () {
             // cancel: function () {} 右上角关闭按钮触发的回调
         });
     });
+
+    //文章状态修改按钮监听
+    $(".myArticle-bottom-articles").on("click",".btn-2",function () {
+        var aid = $(this).attr("name");
+        var status = $(this).attr("value");
+        var articleName = $(".article-item-title[name=" + aid + "]").text();
+        if (status == 5) {
+            //是否下线
+            $(".articleOffline-title").text(articleName);
+            var thisOpen = layer.open({
+                type: 1,    //弹窗类型,2为页面层
+                title: false,   //弹出标题
+                content: $("#articleOffline"),
+                skin: 'modelBg',    //弹窗样式
+                area: ['300px','130px'],  //弹窗大小
+                offset: ['175px','400px'],  //弹窗位置[top,left]，默认auto垂直水平居中
+                closeBtn: 2,     //右上角关闭按钮，有1、2两种样式，0是不显示
+                shade: 0,     //弹层外区域,可自定义样式shade: [0.8, '#393D49']
+                shadeClose: false,  //点击弹层外区域关闭
+                anim: 0,    //弹出动画
+                isOutAnim: true,    //关闭动画
+                fixed: true,    //鼠标滚动时，层是否固定在可视区域
+                resize: false,  //弹窗大小是否可拖动
+                //scrollbar: true   // 是否允许浏览器出现滚动条
+                // cancel: function () {} 右上角关闭按钮触发的回调
+                success: function () {
+                    $("#articleOffline-btn").click(function () {
+                        var data = JSON.stringify({
+                            "status": 1,
+                            "id": aid
+                        });
+                        updateArticleStatus(data, "下线成功！！！", thisOpen);
+                    });
+                }
+            });
+        } else if (status == 2 || status == 7) {
+            //提示
+            var title = "";
+            if (status == 2) {
+                title = "管理员正在审核！！！请耐心等待。"
+            } else {
+                title = "您的文章未通过审核！！！请重新编辑。"
+            }
+            layer.msg(title,{
+                time: 2000,
+                offset: ['375px','425px']
+            });
+        } else {
+            //选择发布
+            $(".republication-title").text(articleName);
+            var thisOpen = layer.open({
+                type: 1,    //弹窗类型,2为页面层
+                title: false,   //弹出标题
+                content: $("#republication"),
+                skin: 'modelBg',    //弹窗样式
+                area: ['300px','150px'],  //弹窗大小
+                offset: ['175px','400px'],  //弹窗位置[top,left]，默认auto垂直水平居中
+                closeBtn: 2,     //右上角关闭按钮，有1、2两种样式，0是不显示
+                shade: 0,     //弹层外区域,可自定义样式shade: [0.8, '#393D49']
+                shadeClose: false,  //点击弹层外区域关闭
+                anim: 0,    //弹出动画
+                isOutAnim: true,    //关闭动画
+                fixed: true,    //鼠标滚动时，层是否固定在可视区域
+                resize: false,  //弹窗大小是否可拖动
+                //scrollbar: true   // 是否允许浏览器出现滚动条
+                // cancel: function () {} 右上角关闭按钮触发的回调
+                success: function () {
+                    $("#republication-btn").click(function () {
+                        var data = JSON.stringify({
+                            "status": 5,
+                            "releaseTime": toDate($("#republicationTime").val(),0),
+                            "id": aid
+                        });
+                        updateArticleStatus(data, "发布成功！！！", thisOpen);
+                    });
+                }
+            });
+        }
+        
+    });
+
+    //文章状态修改
+    function updateArticleStatus(data, hint, thisOpen) {
+        $.ajax({
+            url: path + "/article/article/update",
+            type: "post",
+            data: data,
+            dataType: "json",
+            contentType: "application/json;charset=utf-8",
+            success: function (data) {
+                if (data.ok) {
+                    layer.close(thisOpen);
+                    layer.msg(hint,{
+                        time: 1800,
+                        offset: ['210px','520px']
+                    },function () {
+                        loadMyArticle(null);
+                    });
+                } else {
+                    layer.msg(data.message,{
+                        time: 1800,
+                        offset: ['210px','520px']
+                    });
+                }
+            }
+        });
+    }
 
     //新增标签
     $("#btnAddLabel").click(function () {
@@ -231,16 +342,21 @@ layui.use(['laytpl','layer','laydate'],function () {
 
     laydate.render({
         elem: "#updateStart"
-    })
+    });
 
     laydate.render({
         elem: "#updateEnd"
-    })
+    });
+
+    laydate.render({
+        elem: '#republicationTime',
+        type: 'datetime',
+        value: new Date()
+    });
 
     //悬浮标签出现删除按钮
     var isOut = true;
     $(".class-items").on('mouseenter mouseleave','.class-item',function(){
-        console.log("额",isOut);
         var id = $(this).attr("name");
         if (isOut) {
             $(".del-label-item[name=" + id + "]").show();
@@ -278,6 +394,48 @@ layui.use(['laytpl','layer','laydate'],function () {
         });
     });
 
+    //删除选中的文章
+    $("#del").click(function () {
+        var delNum = 0;
+        var idList = "del";
+        $(".article-item-check").each(function (i, v) {
+            delNum++;
+            idList += "_" + $(v).attr("name");
+        });
+        if (delNum === 0) {
+            layer.msg("请选择要删除的文章！！！",{
+                time: 2000,
+                offset: ['375px','425px']
+            });
+        } else {
+            $("#deleteNum").text(delNum);
+            var thisOpen = layer.open({
+                type: 1,    //弹窗类型,2为页面层
+                title: false,   //弹出标题
+                content: $("#articleDelete"),
+                skin: 'modelBg',    //弹窗样式
+                area: ['300px','150px'],  //弹窗大小
+                offset: ['150px','450px'],  //弹窗位置[top,left]，默认auto垂直水平居中
+                closeBtn: 2,     //右上角关闭按钮，有1、2两种样式，0是不显示
+                shade: 0,     //弹层外区域,可自定义样式shade: [0.8, '#393D49']
+                shadeClose: false,  //点击弹层外区域关闭
+                anim: 0,    //弹出动画
+                isOutAnim: true,    //关闭动画
+                fixed: true,    //鼠标滚动时，层是否固定在可视区域
+                resize: false,  //弹窗大小是否可拖动
+                //scrollbar: true   // 是否允许浏览器出现滚动条
+                // cancel: function () {} 右上角关闭按钮触发的回调
+                success: function () {
+                    $("#articleDelete-btn").click(function () {
+                        var data = JSON.stringify({
+                            "id": idList
+                        });
+                        updateArticleStatus(data, "删除成功！！！", thisOpen);
+                    });
+                }
+            });
+        }
+    });
 
 
 });
